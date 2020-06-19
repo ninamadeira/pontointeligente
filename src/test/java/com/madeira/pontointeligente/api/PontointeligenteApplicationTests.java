9588301c0444af98fb0cd6c5a35fcef175a33664
Nina Rosa Madeira <nina.madeira@gmail.com>
2020-06-19 20:58:34 -0300

    commit inicial dos arquivos do projeto

---
+++ src/test/java/com/madeira/pontointeligente/api/PontointeligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madeira.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontointeligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
